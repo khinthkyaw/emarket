2e2eeb47172c1913cc3e781f07b7d589aafabd87
ktk <khin.th.kyaw@gmail.com>
2026-06-05 22:44:17 +0700

    add Orderdetailpage with Ordersummarypage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,10 @@ import CartPage from "./pages/CartPage";
 import OrdersPage from "./pages/OrdersPage";
 import CheckoutReturnPage from "./pages/CheckoutReturnPage";
 import ProductDetailPage from "./pages/ProductDetailPage";
+import SentryDemoPage from "./pages/SentryDemoPage";
+import OrderDetailPage from "./pages/OrderDetailPage";
+import OrderSummaryPage from "./pages/OrderSummaryPage";
+import OrderChatPage from "./pages/OrderChatPage";
 
 function App() {
   const { isLoaded, isSignedIn } = useAuth();
@@ -20,6 +24,11 @@ function App() {
         <Route path="/product/:slug" element={<ProductDetailPage />} />
         <Route path="/orders" element={isSignedIn ? <OrdersPage /> : <Navigate to={"/"} replace />} />
         <Route path="/checkout/return" element={<CheckoutReturnPage />} />
+        <Route path="/demo-sentry" element={<SentryDemoPage />} />
+        <Route path="/orders/:id" element={<OrderDetailPage />}>
+          <Route index element={<OrderSummaryPage />} />
+          <Route path="chat" element={<OrderChatPage />} />
+        </Route>
       </Routes>
     </Layout>
   )
