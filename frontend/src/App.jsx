e1b7da26dde0ac31e8c8d226d802f79a29273248
ktk <khin.th.kyaw@gmail.com>
2026-06-06 09:38:20 +0700

    add Adminproductspage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ import OrderDetailPage from "./pages/OrderDetailPage";
 import OrderSummaryPage from "./pages/OrderSummaryPage";
 import OrderChatPage from "./pages/OrderChatPage";
 import OrderVideoPage from "./pages/OrderVideoPage";
+import AdminProductsPage from "./pages/AdminProductsPage";
 
 function App() {
   const { isLoaded, isSignedIn } = useAuth();
@@ -27,6 +28,7 @@ function App() {
         <Route path="/checkout/return" element={<CheckoutReturnPage />} />
         <Route path="/demo-sentry" element={<SentryDemoPage />} />
         <Route path="/orders/:id/call" element={isSignedIn ? <OrderVideoPage /> : <Navigate to={"/"} repalce />} />
+        <Route path="/admin" element={isSignedIn ? <AdminProductsPage /> : <Navigate to="/" replace />} />
         <Route path="/orders/:id" element={<OrderDetailPage />}>
           <Route index element={<OrderSummaryPage />} />
           <Route path="chat" element={<OrderChatPage />} />
